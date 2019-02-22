7a5fd51c30a7eca9ee80689b70fee59b205f78f4
Maelle <maelleto.do@gmail.com>
2019-02-22 16:57:06 +0100

    Ajout d'un fichier bdd_connection

--- model/SendMessageModel.php
+++ model/SendMessageModel.php
@@ -1,9 +1,6 @@
 <?php
 
-define("HOST", "dbserver"); // The host to connect to
-define("USER", "tdesbarat001"); // The database username
-define("PASSWORD", "Tristan29!"); // The database password
-define("DATABASE", "tdesbarat001"); // The database name
+include("bdd_connection.php");
 
 class SendMessageModel{
     public function sendMessage($post){
---
+++ model/bdd_connection.php
@@ -0,0 +1,5 @@
+<?php
+define("HOST", "localhost"); // The host to connect to
+define("USER", "root"); // The database username
+define("PASSWORD", "root"); // The database password
+define("DATABASE", "tdesbarat001"); // The database name
