baade77a0a36d1182b8feb1b06779887505a0661
Roxane Flores <rflores@emi.u-bordeaux1.fr>
2019-03-08 17:33:01 +0100

    ajour python

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-import nltk
+f = open("texte_test.txt", 'rU')
 
-from nltk.book import *
-
-len(text3)
-#text1.similar("monstrous")
-#text2.similar("monstrous")
-#text2.generate()
-
-#text3.common_contexts(["father", "Isaac "])
+for ligne in f :
+    phrase = ligne.split()
+    print phrase
+    print len(phrase)
+    for i in range(0,len(phrase)):
+        print phrase[i]
+        if phrase[i] == "moche":
+            print "ce mot est interdit : '%s'. Il faut revoir votre message" % phrase[i]
+            return 1
+return 0
