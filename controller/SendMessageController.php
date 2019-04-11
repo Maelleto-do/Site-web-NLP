bf3b311981f3681cbfecd8fcaa31b9357dbde5d3
Tristan Desbarats <tdesbarat001@emi.u-bordeaux1.fr>
2019-04-11 19:33:42 +0200

    Reparation des conneries de Roxanne

--- controller/SendMessageController.php
+++ controller/SendMessageController.php
@@ -22,56 +22,35 @@ class SendMessageController extends AbstractController {
 
   function __construct($post){
 
-
     $this->modelSubject = new SubjectModel;
     $this->tabCheckSubject = $this->modelSubject->checkSubject($post);
     $this->subjectId = $this->tabCheckSubject['IDSUBJECT'];
-
-
-
     $this->modelPython = new SendMessagePythonModel();
     $this->checkMessageGood = $this->modelPython->SendMessagePython($post);
-
     $this->modelGetID = new GetIDFromBDModel();
     $this->USERID = $this->modelGetID->GetUSERID($post);
     $this->ADMINID = $this->modelGetID->GetADMINID($post);
 
-    $this->modelbis = new MessageModel();
-    $this->modelbis-> setIdSubject($this->tabCheckSubject['IDSUBJECT']);
-    $this->tabGetMessages = $this->modelbis->getMessages($post);
-
-
     if($this->checkMessageGood == 0){
-
       $this->message_erreur = 'Le message envoyé contient un mot interdit.';
-
       $this->view = new SubjectDisplay();
-      $this->view->setMessageisGood($this->message_erreur);
-      $this->view->setCheckSubject($this->tabCheckSubject);
+      $this->view -> setMessageisGood($this->message_erreur);
       $this->view->setGetMessages($this->tabGetMessages);
       $this->view->setADMINID($this->ADMINID);
       $this->view->setUSERID($this->USERID);
-
-
     }else{
-
       $this->model = new SendMessageModel();
       $this->model->getUserId($this->USERID);
       $this->model->setIdSubject($this->subjectId);
       $this->checkMessageSent = $this->model->sendMessage($post);
-
-
-
+      $this->modelbis = new MessageModel();
+      $this->modelbis-> setIdSubject($this->tabCheckSubject['IDSUBJECT']);
+      $this->tabGetMessages = $this->modelbis->getMessages($post);
       $this -> view = new SubjectDisplay();
       $this->view->setCheckSubject($this->tabCheckSubject);
       $this->view->setGetMessages($this->tabGetMessages);
       $this->view->setADMINID($this->ADMINID);
       $this->view->setUSERID($this->USERID);
-
     }
-
-
-
   }
-
 }
